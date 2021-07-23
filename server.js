d2d2be593dbadb88dab3421c6ac9ba389e262feb
Marko Caric <pbmc117@gmail.com>
2021-07-22 23:49:11 -0700

    added 'add employee' option, now action succesfully adds employee. role id and manager id using work around

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 const mysql = require('mysql');
 const inquirer = require("inquirer");
-const { connect } = require('http2');
 require('console.table')
 
-  const menu = () =>{
+const start = () => {
+  console.log(`
+  |-------------------------------------|
+  |                                     |
+  |                                     |
+  |                                     |
+  |           EMPLOYEE TRACKER          |
+  |                                     |
+  |                                     |
+  |                                     |
+  |                                     |
+  |-------------------------------------|
+   
+   
+   `);
+  menu();
+
+}
+const menu = () =>{
     //display choices post,  bid, and exit 
     return inquirer
     .prompt([
@@ -61,22 +78,115 @@ require('console.table')
           
         }).catch((error) => {
           console.log(error);
-         
-        })
-            
-  }
+        })      
+}
           
                   
-    viewAll = () => {
-    const query = 'SELECT * FROM roles';
-    console.log(query)
-    connection.query(query, (err, res) => {
-      if (err) throw err;
-      console.table(res);
-      menu()
-    });
-    
-  };
+viewAll = () => {
+      const query = 'SELECT * FROM employee';
+      console.log(query)
+      connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        menu()
+      });
+};
+
+viewByDepartment = () => {
+      const query = 'SELECT * FROM roles';
+      console.log(query)
+      connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        menu()
+      });
+};
+
+viewByManager = () => {
+     
+      const query = 'SELECT * FROM roles';
+      console.log(query)
+      connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        menu()
+      });
+};
+
+addEmployee = () =>{
+      return inquirer
+      .prompt([{
+        name: 'first_name',
+        type: 'input',
+        message: 'What is employees first name?',
+      },
+      {
+        name: 'last_name',
+        type: 'input',
+        message: 'What is employees last name?',
+      },
+      {
+        name: 'role',
+        type: 'list',
+        message: `'What is employees role?
+1.Sales Lead,
+2.Salesperson,
+3.Lead Engineer,
+4.Software Engineer,
+5.Accountant,
+6.Legal Team Lead,
+7.Lawyer`,
+        choices: [
+          '1',
+          '2',
+          '3',
+          '4',
+          '5',
+          '6',
+          '7',
+        ]
+      },
+      {
+        name: 'manager',
+        type: 'list',
+        message: `'Who is employees Manager?',
+        'John Doe',
+        'Mike Chan',
+        'Ashely Rodriguez',
+        'Kevin Tupik',
+        'Malia Brown',
+        'Sarah Lourd',
+        'Tom Allen',
+        '0'`,
+        choices: [
+          '1',
+          '2',
+          '3',
+          '4',
+          '5',
+          '6',
+          '7',
+          '0'
+        ]
+      }])
+      .then((answers) =>{
+        console.log(answers);
+        const query = `INSERT INTO employee SET ?`
+        connection.query(query, 
+          {
+          first_name: answers.first_name,
+          last_name: answers.last_name,
+          role_id: answers.role || 0,
+          manager_id: answers.manager || 0,
+        },
+        (err) => {
+          if(err) throw err;
+          console.log("The employee was successfully created.");
+          menu();
+        }
+        )
+      })
+    };
                   
                   
                   
@@ -98,6 +208,6 @@ database: 'employee_db',
 connection.connect((err) => {
 if (err) throw err;
 console.log(`connected as id ${connection.threadId}\n`)
-menu();
+start();
 });
 
